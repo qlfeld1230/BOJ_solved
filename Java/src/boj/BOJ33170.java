aa336daa60f2702290c421c29c3b18985876ea34
Duri Park <qlfeld1324@gmail.com>
2025-04-19 15:25:15 +0900

    add BOJ33170.java
    solved BOJ 33170 blackjack

---
+++ Java/src/boj/BOJ33170.java
@@ -0,0 +1,27 @@
+package boj;
+
+import java.io.*;
+
+public class BOJ33170 {
+    public void solve() throws IOException {
+        // 📝 문제 설명
+        /*
+         * 문제: 3개의 입력이 주어졌을 때, 21 이하라면 1을 이상이라면 0을 출력하라.
+         * 
+         * 입력: 각 줄에 순서대로 정수 3개가 주어진다. (1 ≤ A, B, C ≤ 13)
+         * 
+         * 출력: 결과를 출력한다.
+         * 
+         */
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+        int C = Integer.parseInt(br.readLine());
+
+        
+        System.out.println((A + B + C <= 21) ? 1 : 0);
+    }
+}
